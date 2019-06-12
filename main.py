2d1be526e5b5e8c917dfc80481d0f4616b12f514
pedrosena138 <48602553+pedrosena138@users.noreply.github.com>
2019-06-12 18:47:21 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+import misere
+import notakto
+
+
+def main():
+    condicao_jogar = 'S'
+    while condicao_jogar == 'S':
+
+        #Escolhendo o modo de jogo
+        modo_de_jogo = int(input("Escolha um modo de jogo: \n1-Notakto \n2-Misere \nSua escolha: "))
+        while modo_de_jogo != 1 and modo_de_jogo != 2:
+            os.system('cls||clear')
+            modo_de_jogo = int(input("Escolha um modo de jogo: \n1-Notakto \n2-Misere \nSua escolha: "))
+
+        if  modo_de_jogo == 1:
+            os.system('cls||clear')
+            notakto.jogo()
+        else:
+            os.system('cls||clear')
+            misere.jogo()
+
+        condicao_jogar = input("\nQuer continuar jogando? [s/n]: ").upper()
+        while condicao_jogar != 'S' and condicao_jogar != 'N':
+            condicao_jogar = input("Quer continuar jogando? [s/n]: ").upper()
+
+        os.system('cls||clear')
+
+if __name__ == '__main__':
+    os.system('cls||clear')
+    main()
---
+++ notakto.py
@@ -0,0 +1,2 @@
+def jogo():
+    print("jogando notakto")
