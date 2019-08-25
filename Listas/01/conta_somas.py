cc375a30bdf810b25bff935dd722e605fb485b38
pedrosena138 <pmfsl@cin.ufpe.br>
2019-08-25 19:08:18 -0300

    Insert conta_somas.py

---
+++ Listas/01/conta_somas.py
@@ -0,0 +1,49 @@
+'''
+Univesidade Federal de Pernambuco -- UFPE (http://www.ufpe.br)
+Centro de Informatica -- CIn (http://www.cin.ufpe.br)
+Bacharelado em Sistemas de Informacao
+IF969 -- Algoritmos e Estruturas de Dados
+
+Autor:    Antônio Paulino de Lima Neto
+Email:    apln2@cin.ufpe.br
+Data:        2018-03-01
+
+Descricao:  Algoritmo para contagem de triplas em vetores cuja soma seja zero.
+
+
+Licenca: The MIT License (MIT)
+            Copyright(c) 2018 Antônio Paulino de Lima Neto
+'''
+import sys
+import numpy
+
+# Lembre-se de importar a sua classe cronometro
+#from Cronometro import Cronometro
+
+MAX = 999999
+
+def gera_seq_aleatoria(tam):
+   return numpy.random.randint(-MAX,MAX, size=tam)
+
+# Voce deve implementar essa funcao
+def conta_somas(vetor):
+    pass
+
+def main():
+    '''
+    Contem os comandos em Python referentes `a implementacao do algoritmo
+    '''
+    seeds = [11,7,13,19,5189]
+    tam = [50,100,250,500,1000]
+    for i,seed in enumerate(seeds):
+       numpy.random.seed(seed)
+       vetor = gera_seq_aleatoria(tam[i])
+       cron = Cronometro()
+       total = conta_somas(vetor)
+       print("Tempo gasto com {0} elementos foi {1} segundos".format(tam[i],cron))
+       del vetor
+       del cron
+
+
+if __name__ == '__main__':
+    main()
