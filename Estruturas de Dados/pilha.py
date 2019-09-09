5fe9ca0e87ed9e2b10b92b5cc2eff7f210fe4965
pedrosena138 <pmfsl@cin.ufpe.br>
2019-09-09 11:52:02 -0300

    Update pilha.py

---
+++ Estruturas de Dados/pilha.py
@@ -0,0 +1,15 @@
+"""
+Universidade Federal de Pernambuco - UFPE (www.ufpe.br)
+Centro de Informática - CIn (www2.cin.ufpe.br) 
+Bacharelado em Sistemas de Informação 
+IF969 - Algoritmos e Estrutura de Dados 
+Professor: Hansenclever Bassani 
+
+Autor: Pedro Manoel Farias Sena de Lima (pmfsl)
+Email: pmfsl@cin.ufpe.br
+Data:
+
+Copyright © 2019 todos os direitos reservados
+
+Descricao: Implementacao de uma estrutura de dados tipo Pilha
+"""
