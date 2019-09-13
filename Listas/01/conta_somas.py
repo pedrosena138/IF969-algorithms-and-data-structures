4fa8f2995818a29b3e7ea19b2ba9eb232bb33e70
pedrosena138 <pmfsl@cin.ufpe.br>
2019-09-12 23:14:52 -0300

    Update conta-somas.py

--- Listas/01/conta_somas.py
+++ Listas/01/conta_somas.py
@@ -29,10 +29,13 @@ def gera_seq_aleatoria(tam):
 # Voce deve implementar essa funcao
 def conta_somas(vetor):
    quant_somas = int()
-   for i in range(2,len(vetor)):
-      soma =  vetor[i-2] + vetor[i-1] + vetor[i]
-      if soma == 0:
-         quant_somas += 1
+   tam = len(vetor)
+   for i in range(tam-2):
+      for j in range(i+1, tam-1):
+         for k in range(j+1, tam):
+            soma =  vetor[i] + vetor[j] + vetor[k]
+            if soma == 0:
+               quant_somas += 1
    
    return quant_somas
 
