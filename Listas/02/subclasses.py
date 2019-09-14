60755d7ad5cff98f9170b04da92153d401fbe14a
pedrosena138 <pmfsl@cin.ufpe.br>
2019-09-14 17:49:24 -0300

    Update subclasses.py

--- Listas/02/subclasses.py
+++ Listas/02/subclasses.py
@@ -1,15 +1,55 @@
 from lista import ListaDupla
 
 class Fila(ListaDupla):
-    raise NotImplementedError()
+    def __init__(self, item=None):
+        super().__init__(item)
+    
+    def Enqueue(self, item):
+        super().Anexar(item)
+    
+    def Dequeue(self):
+        if not(self.Vazia()):
+            
+            no_proximo = self._ListaDupla__comeco.getProximo()
+            self._ListaDupla__comeco.setProximo(None)
+            if not(no_proximo is None): no_proximo.setAnterior(None)
+            self._ListaDupla__comeco = no_proximo
+
+    def Inserir(self, chave, item):
+        raise NotImplementedError('Fila() nao possui funcao Inserir')
+
+    def Selecionar(self, chave, item):
+        raise NotImplementedError('Fila() nao possui funcao Selecionar()')
 
 class Pilha(ListaDupla):
-    raise NotImplementedError()
+    def __init__(self, item=None):
+        super().__init__(item)
+    
+    def Push(self, item):
+        super().Anexar(item)
+    
+    def Pop(self):
+        if not(self.Vazia()):
+            no_anterior = self._ListaDupla__fim.getAnterior()
+            self._ListaDupla__fim.setAnterior(None)
+            if not(no_anterior is None): no_anterior.setProximo(None)
+            self._ListaDupla__fim = no_anterior
+    
+    def Inserir(self, chave, item):
+        raise NotImplementedError('Pilha() nao possui funcao Inserir()')
+
+    def Selecionar(self, chave, item):
+        raise NotImplementedError('Pilha() nao possui funcao Selecionar()')
 
 def main():
-    fila = Fila()
-    pilha = Pilha()
+    fila = Fila('oi')
+    pilha = Pilha('ola')
 
+    pilha.Push('i')
+    fila.Enqueue('i')
+
+    pilha.Pop()
+    fila.Dequeue()
     print(fila)
     print(pilha)
 
