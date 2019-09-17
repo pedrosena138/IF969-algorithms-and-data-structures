8fda494ecd313518151da3d3fb99dcd38bb10249
pedrosena138 <pmfsl@cin.ufpe.br>
2019-09-16 23:03:45 -0300

    Update fila.py

--- Listas/02/fila.py
+++ Listas/02/fila.py
@@ -11,27 +11,7 @@ Copyright © 2019 todos os direitos reservados
 Descricao: Implementacao de uma Fila.
 '''
 
-from base import ListaDupla
-
-class Fila(ListaDupla):
-    def __init__(self, item=None):
-        super().__init__(item)
-    
-    def Enqueue(self, item):
-        super().Anexar(item)
-    
-    def Dequeue(self):
-        if not(self.Vazia()):
-            no_proximo = self._ListaDupla__comeco.getProximo()
-            self._ListaDupla__comeco.setProximo(None)
-            if not(no_proximo is None): no_proximo.setAnterior(None)
-            self._ListaDupla__comeco = no_proximo
-
-    def Inserir(self, chave, item):
-        raise NotImplementedError('Fila() nao possui funcao Inserir')
-
-    def Selecionar(self, chave, item):
-        raise NotImplementedError('Fila() nao possui funcao Selecionar()')
+from base import Fila
 
 def main():
     fila = Fila('algoritmos')
